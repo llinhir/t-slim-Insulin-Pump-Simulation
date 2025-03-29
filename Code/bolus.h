6b5a40fc537bdcef693069bb11957c533c5565f4
llinhir <jessicaglhill@gmail.com>
2025-03-29 17:45:33 -0400

    bowling h and cpp draft.... we bowling

---
+++ Code/bolus.h
@@ -0,0 +1,41 @@
+#ifndef BOLUS_H
+#define BOLUS_H
+
+//#include "user.h"
+
+class Bolus{
+
+	public:
+        // constructor
+        Bolus();
+
+        // destructor
+        ~Bolus();
+
+        // calculator
+        float bolusCalculation(); // values pulled automatically from the CGM
+        float bolusCalculation(float glucose, int carbs); // or should these just be part of the constructor????????
+        // view calculation
+        // administer bolus??
+
+        // features
+		// quickBolus();
+		// extendedBolus();
+		// cancelBolus();
+		// stopOngoingBolus();
+
+        // getters
+        float getGlucose(){return currGlucose;};
+        int getCarbs(){return carbohydrates;};
+
+        // setters
+        void setGlucose(float glucose){currGlucose = glucose;};
+        void setCarbs(int carbs){carbohydrates = carbs;};
+		
+	private:
+        float currGlucose; // mmol/L
+        int carbohydrates; // grams
+
+}; // Bolus class
+
+#endif
